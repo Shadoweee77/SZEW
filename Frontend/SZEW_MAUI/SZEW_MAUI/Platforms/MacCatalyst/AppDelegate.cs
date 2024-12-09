eba11b0c14cce98f46797f51a73a661d1a7291ec
Shadoweee77 <itzshadowitzshadow@gmail.com>
2024-12-09 23:41:25 +0100

    (add) Frontend: MAUI init

---
+++ Frontend/SZEW_MAUI/SZEW_MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SZEW_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
