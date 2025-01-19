95163e21fe6d60aaa89e7d4bbf2857eff1715d1e
Adrian Bachmura <115699706+AdrianRev@users.noreply.github.com>
2025-01-19 20:19:25 +0100

    (fix) Backend: added missing Id's from get methods

--- Backend/SZEW/SZEW/DTO/SparePartDto.cs
+++ Backend/SZEW/SZEW/DTO/SparePartDto.cs
@@ -2,6 +2,7 @@
 {
     public class SparePartDto
     {
+        public required int Id { get; set; }
         public required string Name { get; set; }
         public double Price { get; set; }
         public int Amount { get; set; }
--- Backend/SZEW/SZEW/DTO/ToolDto.cs
+++ Backend/SZEW/SZEW/DTO/ToolDto.cs
@@ -2,6 +2,7 @@
 {
     public class ToolDto
     {
+        public required int Id { get; set; }
         public required string Name { get; set; }
         public double Price { get; set; }
         public int Amount { get; set; }
